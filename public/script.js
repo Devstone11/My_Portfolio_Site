1ba04921284476c2e1ca00337d7b4963f579ef74
Devin Stone <devstone11@gmail.com>
2016-08-29 15:12:26 -0600

    scroll effects and favicon added

--- public/script.js
+++ public/script.js
@@ -1,7 +1,23 @@
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 500) {
-    $('nav').addClass('shrink');
-  } else {
-    $('nav').removeClass('shrink');
-  }
+
+
+$(document).ready(function (){
+  $("#about-link").click(function (){
+    $(window).scrollTo($("#about"), 800);
+  });
+
+  $("#projects-link").click(function (){
+    $(window).scrollTo($("#projects"), 800);
+  });
+
+  $("#contact-link").click(function (){
+    $(window).scrollTo($("#contact"), 800);
+  });
+
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > 500) {
+      $('nav').addClass('shrink');
+    } else {
+      $('nav').removeClass('shrink');
+    }
+  });
 });
